e585b900fcd6503b5cd0188054a5315673559029
Moyinoluwa-10 <moyinadelowo@gmail.com>
2022-10-13 22:05:47 +0100

    Update

--- src/Pages/LandingPage/Landingpage.js
+++ src/Pages/LandingPage/Landingpage.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 const Landingpage = () => {
-  return <div>Landingpage</div>;
+  return <div>Landingpage
+    <h1>How are you</h1>
+    <div>Helsjgsah</div>
+  </div>;
 };
 
 export default Landingpage;
