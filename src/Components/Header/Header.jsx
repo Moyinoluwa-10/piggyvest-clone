8310c7e60200a76c362ee531cdca29ff255f5bdc
Stephen <stephengift43@gmail.com>
2022-10-16 14:34:20 +0100

    Revert "Header responsiveness"
    This reverts commit 09e7eafd3c6653d12079920b57d17e5d8ec99212.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,52 +1,37 @@
-import React from "react";
-import { useState } from "react";
-import "./Header.css";
-import PiggyvestLogo from "../../images/piggyvest-logo.svg";
+import React from 'react'
+import './Header.css'
+import PiggyvestLogo from '../../images/piggyvest-logo.svg';
 import { Link } from "react-router-dom";
 
-import { VscThreeBars, VscClose } from "react-icons/vsc";
 
 const Header = () => {
-  const [responsive, setResponsive] = useState(false);
-  const handleClick = () => {
-    setResponsive(!responsive);
-  };
-
   return (
-    <div className="headerpage">
-      <header className={responsive ? "header Expanded" : "header"}>
-     
-        <div className="header-container">
-          <div className="header-logo">
-            <img src={PiggyvestLogo} alt="PiggyVest Logo" />
-          </div>
+    <div className='headerpage'>
 
-          <div className="navBtn">
-            <div className="header-link">
-              <Link to={"/"}>Save</Link>
-              <Link to={"/"}>Invest</Link>
-              <Link to={"/"}>Stories</Link>
-              <Link to={"/"}>FAQs</Link>
-              <Link to={"/"}>Blog</Link>
+        <header className="header">
+            <div className="header-container">
+                <div className="header-box">
+                <div className="header-logo">
+                    <img src={PiggyvestLogo} alt="PiggyVest Logo" />
+                </div>
+                <div className="header-link">
+                    <Link to={'/'}>Save</Link> 
+                    <Link to={'/'}>Invest</Link> 
+                    <Link to={'/'}>Stories</Link> 
+                    <Link to={'/'}>FAQs</Link> 
+                    <Link to={'/'}>Blog</Link> 
+                </div>
+                </div>
+
+                <div className="nav-link">
+                <Link to={'/login'} className="btn btn-login">Sign in</Link>
+                <Link to={'/signup'} className="btn btn-signup">Create free account</Link>
+                </div>
             </div>
+        </header>
 
-            <div className="nav-link">
-              <Link to={"/login"} className="btn btn-login">
-                Sign in
-              </Link>
-              <Link to={"/signup"} className="btn btn-signup">
-                Create free account
-              </Link>
-            </div>
-          </div>
-
-          <div className="toogleIcons" onClick={handleClick}>
-            {responsive ? <VscClose /> : <VscThreeBars />}
-          </div>
-        </div>
-      </header>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
